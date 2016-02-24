07194831c1514424791052aed18e863e0920602c
Coit Scharringhausen <coit.scharringhausen@gmail.com>
2016-02-24 12:38:03 -0600

    Refactor User class and Date class. Implement date_format_book dictionary. Remove AmericanDate class.

--- view_user_birthdate.py
+++ view_user_birthdate.py
@@ -6,6 +6,53 @@ that user's formatted birthdate.
 import re
 from get_input import get_input
 
+REGEX_DATE_RULE = ("(?P<month>[0-9]{1,2})" # Month.
+                   "/(?P<day>[0-9]{1,2})" # Day.
+                   "/(?P<year>[0-9]{4})" # Year.
+                  )
+
+class Date(object):
+    """Take in a string in the format:
+    mm/dd/yyyy
+    Create a Date object from that string with the attributes month,
+    day, and year.
+    """
+    def __init__(self, string):
+        """Instantiate a Date object with the attributes 'month',
+        'day', and 'year'.
+        """
+        assert_response = "A string in the format mm/dd/yyyy must be provided."
+        assert re.search(REGEX_DATE_RULE, string), assert_response
+        self.month, self.day, self.year = parse_date(string)
+
+    def display_date(self):
+        """Return a string in the format:
+        mm/dd/yyyy
+        """
+        return "%s/%s/%s" % (self.month, self.day, self.year)
+
+class EuropeanDate(Date):
+    """Create an object of type EuropeanDate which inherits from class
+    Date.
+    """
+    def display_date(self):
+        """Using the attributes 'month', 'day', and 'year', return a
+        string in the European format:
+        dd/mm/yyyy
+        """
+        return '%s/%s/%s' % (self.day, self.month, self.year)
+
+class ChineseDate(Date):
+    """Create an object of type ChineseDate wich inherits from class
+    Date.
+    """
+    def display_date(self):
+        """Using the attributes 'month', 'day', and 'year', return a
+        string in the Chinese format:
+        yyyy/mm/dd
+        """
+        return '%s/%s/%s' % (self.year, self.month, self.day)
+
 class User(object):
     """ Take in two strings 'username' and 'nationality' and a Date
     'birthdate'. Create an object of type User with those same
@@ -24,152 +71,101 @@ class User(object):
         self.nationality = nationality.lower()
         self.birthdate = Date(birthdate)
 
-    date_format_by_nationality = {
-        'american': AmericanDate,
-        'belizean': AmericanDate,
-        'micronesian': AmericanDate,
-        'british': EuropeanDate,
-        'irish': EuropeanDate,
-        'german': EuropeanDate,
-        'italian': EuropeanDate,
-        'spanish': EuropeanDate,
-        'australian': EuropeanDate,
-        'new zealander': EuropeanDate,
-        'south african': EuropeanDate,
-        'brazilian': EuropeanDate,
-        'mexican': EuropeanDate,
-        'indian': EuropeanDate,
-        'indonesian': EuropeanDate,
-        'nigerian': EuropeanDate,
-        'russian': EuropeanDate,
-        'canadian': EuropeanDate,
-        'french': EuropeanDate,
-        'chinese': ChineseDate,
-        'north korean': ChineseDate,
-        'south korean': ChineseDate,
-        'japanese': ChineseDate,
-        'taiwanese': ChineseDate,
-        'hungarian': ChineseDate,
-        'lithuanian': ChineseDate,
-        'iranian': ChineseDate
-    }
+    def print_birthdate(self):
+        """Print the User's birthdate."""
+        birthdate = self.format_birthdate()
+        print birthdate.display_date()
 
-    def print_user_birthdate(self):
-        assert self.nationality in date_format_by_nationality, "Nationality unknown."
-        print date_format_by_nationality[self.nationality](self.birthdate).format_date()
+    def format_birthdate(self):
+        """Format the User's birthdate according to it's nationality."""
+        date_format_book = {
+            'american': Date,
+            'belizean': Date,
+            'micronesian': Date,
+            'british': EuropeanDate,
+            'irish': EuropeanDate,
+            'german': EuropeanDate,
+            'italian': EuropeanDate,
+            'spanish': EuropeanDate,
+            'australian': EuropeanDate,
+            'new zealander': EuropeanDate,
+            'south african': EuropeanDate,
+            'brazilian': EuropeanDate,
+            'mexican': EuropeanDate,
+            'indian': EuropeanDate,
+            'indonesian': EuropeanDate,
+            'nigerian': EuropeanDate,
+            'russian': EuropeanDate,
+            'canadian': EuropeanDate,
+            'french': EuropeanDate,
+            'chinese': ChineseDate,
+            'north korean': ChineseDate,
+            'south korean': ChineseDate,
+            'japanese': ChineseDate,
+            'taiwanese': ChineseDate,
+            'hungarian': ChineseDate,
+            'lithuanian': ChineseDate,
+            'iranian': ChineseDate
+        }
+        assert self.nationality in date_format_book, "Nationality format unknown."
+        return date_format_book[self.nationality](self.birthdate.display_date())
 
-class Date(object):
-    """Take in a string in the format:
-    mm/dd/yyyy
-    Create a Date object from that string with the attributes month,
-    day, and year.
+def parse_date(string):
+    """Take in a string containing a date in the format mm/dd/yyyy and
+    return three strings representing a valid day, month, and year.
     """
-    regex_rule = ("(?P<month>[0-9]{1,2})" # Month.
-                  "/(?P<day>[0-9]{1,2})" # Day.
-                  "/(?P<year>[0-9]{4})" # Year.
-                 )
+    date = re.search(REGEX_DATE_RULE, string)
+    assert date, "A string in the format mm/dd/yyyy must be provided."
 
-    def __init__(self, string):
-        """Instantiate a Date object with the attributes 'month',
-        'day', and 'year'.
-        """
-        assert re.search(Date.regex_rule, string), "A string in the format: mm/dd/yyyy must be provided."
-        self.month, self.day, self.year = self.parse_date(string)
+    month = make_int(date.group('month'))
+    day = make_int(date.group('day'))
+    year = make_int(date.group('year'))
 
-    def parse_date(self, string):
-        """Take in a string containing a date in the format mm/dd/yyyy and
-        return three strings representing a valid day, month, and year.
-        """
-        date = re.search(Date.regex_rule, string)
-        assert date, "A string in the format mm/dd/yyyy must be provided."
+    assert is_valid_date(month, day, year), "A valid date must be provided."
 
-        month = self.make_int(date.group('month'))
-        day = self.make_int(date.group('day'))
-        year = self.make_int(date.group('year'))
+    return str(month), str(day), str(year)
 
-        assert self.is_valid_date(month, day, year), "A valid date must be provided."
-
-        return str(month), str(day), str(year)
-
-    def make_int(self, string):
-        """Convert a given string to an integer. If it cannot be converted,
-        return None.
-        """
-        try:
-            return int(string)
-        except ValueError:
-            return None
-
-    def is_valid_date(self, month, day, year):
-        """Take in three integers representing a month, day, and year.
-        Verify they are valid representations and return True or False.
-        """
-        return self.valid_month(month) and self.valid_day(day, month) and self.valid_year(year)
-
-    def valid_month(self, month):
-        """Take in an integer representing a month of the year. Return True
-        if it is valid. Otherwise, return False.
-        """
-        return 1 <= month <= 12
-
-    def valid_day(self, day, month):
-        """Take in an integer representing a day and month of the year.
-        Return True if it is a valid day for the given month. Otherwise,
-        return False.
-        """
-        month31 = [1, 3, 5, 7, 8, 10, 12]
-        month30 = [4, 6, 9, 11]
-        assert self.valid_month(month)
-        if month in month31:
-            return 1 <= day <= 31
-        if month in month30:
-            return 1 <= day <= 30
-        if month == 2:
-            return 1 <= day <= 28
-
-    def valid_year(self, year):
-        """Take in an integer representing a pertinent year. Return True if
-        it is valid. Otherwise, return False.
-        """
-        return 1800 <= year <= 2200
-
-    def print_date(self):
-        print "Month = %s" % self.month
-        print "Day = %s" % self.day
-        print "Year = %s" % self.year
-
-class AmericanDate(Date):
-    """Create an object of type AmericanDate wich inherits from class
-    Date.
+def make_int(string):
+    """Convert a given string to an integer. If it cannot be converted,
+    return None.
     """
-    def format_date(self):
-        """Using the attributes 'month', 'day', and 'year', return a
-        string in the American format:
-        mm/dd/yyyy
-        """
-        return '%s/%s/%s' % (self.month, self.day, self.year)
+    try:
+        return int(string)
+    except ValueError:
+        return None
 
-class EuropeanDate(Date):
-    """Create an object of type EuropeanDate which inherits from class
-    Date.
+def is_valid_date(month, day, year):
+    """Take in three integers representing a month, day, and year.
+    Verify they are valid representations and return True or False.
     """
-    def format_date(self):
-        """Using the attributes 'month', 'day', and 'year', return a
-        string in the European format:
-        dd/mm/yyyy
-        """
-        return '%s/%s/%s' % (self.day, self.month, self.year)
+    return valid_month(month) and valid_day(day, month) and valid_year(year)
 
-class ChineseDate(Date):
-    """Create an object of type ChineseDate wich inherits from class
-    Date.
+def valid_month(month):
+    """Take in an integer representing a month of the year. Return True
+    if it is valid. Otherwise, return False.
     """
-    def format_date(self):
-        """Using the attributes 'month', 'day', and 'year', return a
-        string in the Chinese format:
-        yyyy/mm/dd
-        """
-        return '%s/%s/%s' % (self.year, self.month, self.day)
+    return 1 <= month <= 12
+
+def valid_day(day, month):
+    """Take in an integer representing a day and month of the year.
+    Return True if it is a valid day for the given month. Otherwise,
+    return False.
+    """
+    month31 = [1, 3, 5, 7, 8, 10, 12]
+    month30 = [4, 6, 9, 11]
+    assert valid_month(month)
+    if month in month31:
+        return 1 <= day <= 31
+    if month in month30:
+        return 1 <= day <= 30
+    if month == 2:
+        return 1 <= day <= 28
+
+def valid_year(year):
+    """Take in an integer representing a pertinent year. Return True if
+    it is valid. Otherwise, return False.
+    """
+    return 1800 <= year <= 2200
 
 def read_file(file_name):
     """Read in data from a file and return it."""
@@ -208,6 +204,7 @@ def build_bday_book(file_name):
     build a dictionary where the keys of the dictionary are the
     usernames and the values are the corresponding birthdates.
     """
+    bday_book = {}
     data = read_file(file_name)
     assert data, "There is no data!"
     data_chunks = chunk_data(data, 3)
@@ -218,18 +215,16 @@ def build_bday_book(file_name):
         birthdate = parse("birthdate:(?P<birthdate>[0-9]{1,2}/[0-9]{1,2}/[0-9]{4})",
                           'birthdate', chunk[2])
         if username and nationality and birthdate:
-            month, day, year = build_date(birthdate)
-            birthdate = format_date(month, day, year, nationality)
             bday_book[username] = User(username, nationality, birthdate)
     return bday_book
 
 if __name__ == "__main__":
     FILE = 'user_data.txt' # Enter filename here.
     BDAY_BOOK = build_bday_book(FILE)
-    INPUT = get_input().lower()
-    while not INPUT.isspace() and INPUT:
-        if INPUT in BDAY_BOOK:
-            BDAY_BOOK.print_user_birthdate(INPUT)
+    USERNAME = get_input().lower()
+    while not USERNAME.isspace() and USERNAME:
+        if USERNAME in BDAY_BOOK:
+            BDAY_BOOK[USERNAME].print_birthdate()
         else:
             print "Sorry, user not found."
-        INPUT = get_input().lower()
+        USERNAME = get_input().lower()
